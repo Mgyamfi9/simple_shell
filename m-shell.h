4dd901f34b3e7a5a18364cc161c1b23f40406ce1
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-09-13 01:57:40 -0700

    added header file and write file

---
+++ m-shell.h
@@ -0,0 +1,9 @@
+#ifndef M-SHELL_H
+#define M-SHELL_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+void _write(const char *text);
+#endif
---
+++ m-write.c
@@ -0,0 +1,9 @@
+#include "m-shell.h"
+/**
+ * _write - prints text to the stdoutput
+ * @text: text to print
+ */
+void _write(const char *text)
+{
+	write(STDOUT_FILENO, text, strlen(text));
+}
