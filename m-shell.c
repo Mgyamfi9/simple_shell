0c0550bd2010b94bf7c9883c4d019fa3be98f79b
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-09-18 08:03:00 -0700

    handle exit

--- m-shell.c
+++ m-shell.c
@@ -11,7 +11,11 @@ int main(void)
 	{
 		_prompt();
 		read_input(input, sizeof(input));
-		_execute(input);
+		_execute(input, envp);
+		if (strcmp(input, "exit") == 0)
+		{
+			exit(0);
+	       	}
 	}
 	return (0);
 }
