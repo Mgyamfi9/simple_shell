5ba8ab05dbf99b69f34c0fb59113ca952afed792
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-09-18 07:53:50 -0700

    updated execve

--- m-execute.c
+++ m-execute.c
@@ -2,8 +2,9 @@
 /**
  * _execute - function to execute
  * @order: command to execute
+ * @envp: environment variable
  */
-void _execute(const char *order)
+void _execute(const char *order, char *envp[])
 {
 	pid_t _childpid = fork();
 
@@ -15,9 +16,13 @@ void _execute(const char *order)
 	else if (_childpid == 0)
 	{
 		char *arguments[1024];
+		/*char *directory = "/bin";*/
 		int arg_number = 0;
-
 		char *token = strtok((char *)order, " ");
+		char *exec_name;
+		char full_path[256];
+		char *path;
+		char *path_token;
 
 		while (token != NULL)
 		{
@@ -25,8 +30,44 @@ void _execute(const char *order)
 			token = strtok(NULL, " ");
 		}
 		arguments[arg_number] = NULL;
-		execvp(arguments[0], arguments);
-		_write("Error executing command.\n");
+
+		exec_name = strrchr(arguments[0],'/');
+		if (exec_name == NULL)
+		{
+			exec_name = arguments[0];
+		}
+		else
+		{
+			exec_name++;
+		}
+		path = getenv("PATH");
+		if (path == NULL)
+		{
+			fprintf(stderr, "Error: PATH env is not set.\n");
+			exit(EXIT_FAILURE);
+		}
+		path_token = strtok(path, ":");
+		while (path_token != NULL)
+		{
+			snprintf(full_path, sizeof(full_path), "%s/%s", path_token, exec_name);
+			if (access(full_path, X_OK) == 0)
+			{
+				break;
+			}
+			path_token = strtok(NULL, ":");
+		}
+	       	if (path_token == NULL)
+		{
+			fprintf(stderr, "Error: %s not found in PATH.\n", exec_name);
+			exit(EXIT_FAILURE);
+		}
+		if (execve(full_path, arguments, envp) == -1)
+		{
+			perror("execve");
+			fprintf(stderr, "Error executing command. Path: %s\n", exec_name);
+			exit(EXIT_FAILURE);
+		}
+		fprintf(stderr, "Error executing command.\n");
 		exit(EXIT_FAILURE);
 	}
 	else
--- m-shell.c
+++ m-shell.c
@@ -5,13 +5,22 @@
  */
 int main(void)
 {
+	char *envp[] = {
+		"PATH=/usr/local/bin:/usr/bin:/bin",
+		"SHELL=/bin/bash",
+		NULL
+	};
 	char input[1024];
 
 	while (true)
 	{
 		_prompt();
 		read_input(input, sizeof(input));
-		_execute(input);
+		_execute(input, envp);
+		if (strcmp(input, "exit") == 0)
+		{
+			exit(0);
+		}
 	}
 	return (0);
 }
